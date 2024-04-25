a69c10ff5fd9a4f2dac5736138457958c8045a57
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2024-04-25 15:45:15 +0530

    Cors added

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, Response
 from dtypes import make_response
 from controllers import order_router 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(order_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/healthz")
 def health_check(response: Response):
